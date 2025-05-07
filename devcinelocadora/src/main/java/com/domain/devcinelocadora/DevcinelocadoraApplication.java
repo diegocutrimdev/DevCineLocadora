72e5e84c4e20a941e871cbd03a0f461b587899d7
dicutrim <diegocutrimcontato@gmail.com>
2025-05-06 23:22:24 -0300

    Commit inicial de criação do projeto

---
+++ devcinelocadora/src/main/java/com/domain/devcinelocadora/DevcinelocadoraApplication.java
@@ -0,0 +1,13 @@
+package com.domain.devcinelocadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcinelocadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevcinelocadoraApplication.class, args);
+	}
+
+}
---
+++ devcinelocadora/src/test/java/com/domain/devcinelocadora/DevcinelocadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domain.devcinelocadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevcinelocadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
